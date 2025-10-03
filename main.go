ddae9e7108b845dbfe4aea42af19fda30db3e642
Jhonatan-Code-dev <182036744+Jhonatan-Code-dev@users.noreply.github.com>
2025-10-03 00:02:22 -0500

    update

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 
 	"app-futbol/di"
 	_ "app-futbol/docs"
+	"app-futbol/migrations"
 	"app-futbol/src/routes"
 
 	"github.com/gofiber/fiber/v2"
@@ -12,19 +13,11 @@ import (
 )
 
 func main() {
-
 	container := di.InitializeApp()
-
+	migrations.RunMigrations(container.DB)
 	app := fiber.New()
-
 	routes.SetupRoutes(app, container)
-
 	app.Get("/docs/*", swagger.HandlerDefault)
-
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("¡Hola! La API está funcionando 🚀")
-	})
-
 	log.Printf("🚀 Servidor iniciado en http://localhost:%s", container.Config.Port)
 	log.Fatal(app.Listen(":" + container.Config.Port))
 }
