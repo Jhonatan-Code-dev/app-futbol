912240bfd02a77c49a14446c51489aa19ebc712f
Jhonatan-Code-dev <182036744+Jhonatan-Code-dev@users.noreply.github.com>
2025-10-04 00:37:05 -0500

    s

--- src/schemas/RolPermiso.go
+++ src/schemas/RolPermiso.go
@@ -1,12 +1,12 @@
 package schemas
 
 type RolPermiso struct {
-	IdRolPermiso uint `gorm:"primaryKey;autoIncrement" json:"id_rol_permiso"`
+	IdRolPermiso uint `gorm:"primaryKey;autoIncrement"`
 
-	IDRol     uint `gorm:"not null" json:"id_rol"`
-	IDPermiso uint `gorm:"not null" json:"id_permiso"`
+	IDRol     uint `gorm:"not null"`
+	IDPermiso uint `gorm:"not null"`
 
 	// Relaciones opcionales para hacer Preload
-	Rol     *Rol     `gorm:"foreignKey:IDRol;references:IdRol" json:"rol,omitempty"`
-	Permiso *Permiso `gorm:"foreignKey:IDPermiso;references:IdPermiso" json:"permiso,omitempty"`
+	Rol     *Rol     `gorm:"foreignKey:IDRol;references:IdRol"`
+	Permiso *Permiso `gorm:"foreignKey:IDPermiso;references:IdPermiso"`
 }
