de38660bf1253aa6cf8fe3cff4fc1e9f7927bf0a
Jhonatan-Code-dev <182036744+Jhonatan-Code-dev@users.noreply.github.com>
2025-10-04 00:38:44 -0500

    c

--- src/schemas/Fecha.go
+++ src/schemas/Fecha.go
@@ -3,7 +3,7 @@ package schemas
 import "time"
 
 type Fecha struct {
-	IdFecha       uint      `gorm:"primaryKey;autoIncrement" json:"id_fecha"`
-	FechaApertura time.Time `gorm:"type:datetime;not null" json:"fecha_apertura"`
-	FechaCierre   time.Time `gorm:"type:datetime;not null" json:"fecha_cierre"`
+	IdFecha       uint      `gorm:"primaryKey;autoIncrement"`
+	FechaApertura time.Time `gorm:"type:datetime;not null"`
+	FechaCierre   time.Time `gorm:"type:datetime;not null"`
 }
--- src/schemas/Pago.go
+++ src/schemas/Pago.go
@@ -1,11 +1,11 @@
 package schemas
 
 type Pago struct {
-	IdPago       uint    `gorm:"primaryKey;autoIncrement" json:"id_pago"`
-	IDAsistencia uint    `gorm:"not null" json:"id_asistencia"`
-	IDUsuario    uint    `gorm:"not null" json:"id_usuario"`
-	IDTipoPago   uint    `gorm:"not null" json:"id_tipo_pago"`
-	Pago         float64 `gorm:"not null" json:"pago"`
+	IdPago       uint    `gorm:"primaryKey;autoIncrement"`
+	IDAsistencia uint    `gorm:"not null"`
+	IDUsuario    uint    `gorm:"not null"`
+	IDTipoPago   uint    `gorm:"not null"`
+	Pago         float64 `gorm:"not null"`
 
 	// Relaciones
 	Asistencia Asistencia `gorm:"foreignKey:IDAsistencia;references:IdAsistencia"`
